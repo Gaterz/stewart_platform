fc7493d5cd2459bb26cfb6921f3e06ee6dc0d5b5
gaterz <julien.maurat@free.fr>
2015-04-15 19:04:58 +0200

    +imgrecog : ajout de l'init +imgrecog : ajout de l'exit +imgrecog : ajout du process de base
    Signed-off-by: julien <julien.maurat@insa-rennes.fr>

--- src/imgRecognition.c
+++ src/imgRecognition.c
@@ -6,7 +6,42 @@
  */
 
 #include "mainHeaders.h"
+int thresh_hue_min=20;
+CvCapture* capture_device=NULL;
+void init_imgrecog(int device)
+{
+	CvCapture * cap =cvCaptureFromCAM(device);
+	if(cap==NULL)
+	{
+		perror("unable to open cam");
+		exit(EXIT_FAILURE);
+	}
+	cvNamedWindow("screen",CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("mat0",CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("mat1",CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("mat2",CV_WINDOW_AUTOSIZE);
+	capture_device=cap;
+	cvCreateTrackbar("thresh1", "screen", &thresh_hue_min, 1024,NULL);
+}
 
+void exit_imgrecog()
+{
+	cvReleaseCapture(&capture_device);
+	cvDestroyWindow("screen");
+}
+void process_recog()
+{
+	IplImage* capture=cvQueryFrame(capture_device);
+	cvCvtColor(capture,capture,CV_RGB2HSV);
+	IplImage* arr[4];
+	arr[0]=cvCreateImage(cvGetSize(capture), IPL_DEPTH_8U, 1);
+	arr[1]=cvCreateImage(cvGetSize(capture), IPL_DEPTH_8U, 1);
+	arr[2]=cvCreateImage(cvGetSize(capture), IPL_DEPTH_8U, 1);
+	cvSplit(capture,arr[0],arr[1],arr[2],0);
+	cvThreshold(arr[0],arr[0],thresh_hue_min,255,CV_THRESH_BINARY);
 
-
-
+	cvShowImage("screen",capture);
+	cvShowImage("mat0",arr[0]);
+	cvShowImage("mat1",arr[1]);
+	cvShowImage("mat2",arr[2]);
+}
