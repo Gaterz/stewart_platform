a92d14832accb5776eb61343da2bc9579085bdb7
gaterz <julien.maurat@free.fr>
2015-04-16 14:39:50 +0200

    %img recog : test de la fonction inrange pour le threshold
    Signed-off-by: julien <julien.maurat@insa-rennes.fr>

--- src/imgRecognition.c
+++ src/imgRecognition.c
@@ -7,6 +7,7 @@
 
 #include "mainHeaders.h"
 int thresh_hue_min=20;
+int thresh_hue_max=20;
 CvCapture* capture_device=NULL;
 void init_imgrecog(int device)
 {
@@ -16,12 +17,15 @@ void init_imgrecog(int device)
 		perror("unable to open cam");
 		exit(EXIT_FAILURE);
 	}
+	capture_device=cap;
+
 	cvNamedWindow("screen",CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("mat0",CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("mat1",CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("mat2",CV_WINDOW_AUTOSIZE);
-	capture_device=cap;
-	cvCreateTrackbar("thresh1", "screen", &thresh_hue_min, 1024,NULL);
+
+	cvCreateTrackbar("thresh1", "screen", &thresh_hue_min, 255,NULL);
+	cvCreateTrackbar("thresh2", "screen", &thresh_hue_max, 255,NULL);
 }
 
 void exit_imgrecog()
@@ -38,7 +42,10 @@ void process_recog()
 	arr[1]=cvCreateImage(cvGetSize(capture), IPL_DEPTH_8U, 1);
 	arr[2]=cvCreateImage(cvGetSize(capture), IPL_DEPTH_8U, 1);
 	cvSplit(capture,arr[0],arr[1],arr[2],0);
-	cvThreshold(arr[0],arr[0],thresh_hue_min,255,CV_THRESH_BINARY);
+
+	cvInRangeS(arr[0],cvScalar(thresh_hue_min,0,0,0),cvScalar(thresh_hue_max,0,0,0),arr[0]);
+	//cvThreshold(arr[0],arr[0],thresh_hue_min,255,CV_THRESH_BINARY);
+	//cvThreshold(arr[0],arr[0],thresh_hue_min,255,CV_THRESH_BINARY);
 
 	cvShowImage("screen",capture);
 	cvShowImage("mat0",arr[0]);
