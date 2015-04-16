91e1f4a746f7aeeecb7b09c7a9e3bd8ebd9148dc
jyngle <maxime.selles@gmail.com>
2015-04-16 17:32:47 +0200

    Nettoyage de main.c

--- src/main.c
+++ src/main.c
@@ -6,58 +6,11 @@
  */
 
 
-#include <fcntl.h>
-#include <unistd.h>
-
-#include <gtk/gtk.h>
-
-
-
 #include "main.h"
 
-#define pi  3.14159
-#define DEG_TO_RAD(X) (X*pi/180.0)
-
-int main(int argc, char **argv)
+int main(void)
 {
-	float PositionPlatforme[] = {0,0,0,0,0,0};//= { Posx, Posy, Posz, Rotx, Roty, Rotz }
-	float AngleServos[6];
-	unsigned char AnglesFormate[6];
-	int fileDesc;
 
-
-	initReverseKinematics();
-	fileDesc = openPort("/dev/ttyACM0");
-
-	if(fileDesc == -1)
-	{
-		printf("Impossible d'ouvrir le port !! \n");
-		return 0;
-	}
-
-
-	initPort(fileDesc, 9600);
-
-	positionPlatforme2Anges(PositionPlatforme, AngleServos);
-	formatDonneesGtkWidget(AngleServos, AnglesFormate);
-	sendAngles(fileDesc, AnglesFormate);
-
-	usleep(200);
-
-	close(fileDesc);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	return 1;
+	return 0;
 }
+
