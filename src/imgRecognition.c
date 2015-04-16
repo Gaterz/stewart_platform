c65f0185efe692312f72aa8a496d4dda168a01a8
gaterz <julien.maurat@free.fr>
2015-04-16 19:36:27 +0200

    + imgrecog : ajout de l'ecrant de debug "end_recog" % smooth sur la capture principale
    Signed-off-by: julien <julien.maurat@insa-rennes.fr>

--- src/imgRecognition.c
+++ src/imgRecognition.c
@@ -38,6 +38,7 @@ void init_imgrecog(int device)
 	cvNamedWindow("mat0",CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("mat1",CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("mat2",CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("end_recog",CV_WINDOW_AUTOSIZE);
 	#endif
 }
 
@@ -51,29 +52,33 @@ void exit_imgrecog()
 	cvDestroyWindow("mat0");
 	cvDestroyWindow("mat1");
 	cvDestroyWindow("mat2");
+	cvDestroyWindow("end_recog");
 	#endif
 }
 void process_recog()
 {
 	IplImage* hsv_split[3];
+	IplImage* endRecog;
 	IplImage* capture=cvQueryFrame(capture_device);
 	IplImage* capture_hsv=	cvCreateImage(cvGetSize(capture),capture->depth,capture->nChannels);
 
+	cvSmooth(capture,capture,CV_GAUSSIAN,gauss_size*2+1,gauss_size*2+1,0,0);
 
 	cvCvtColor(capture,capture_hsv,CV_RGB2HSV);
 
 	hsv_split[0]=cvCreateImage(cvGetSize(capture_hsv), IPL_DEPTH_8U, 1);
 	hsv_split[1]=cvCreateImage(cvGetSize(capture_hsv), IPL_DEPTH_8U, 1);
 	hsv_split[2]=cvCreateImage(cvGetSize(capture_hsv), IPL_DEPTH_8U, 1);
+	endRecog=cvCreateImage(cvGetSize(capture_hsv), IPL_DEPTH_8U, 1);
 
-	cvSmooth(capture,capture,CV_GAUSSIAN,gauss_size,gauss_size,0,0);
 	cvSplit(capture_hsv,hsv_split[0],hsv_split[1],hsv_split[2],0);
 
 
 	cvInRangeS(hsv_split[0],cvScalar(thresh_hue_min,0,0,0),cvScalar(thresh_hue_max,0,0,0),hsv_split[0]);
 	cvInRangeS(hsv_split[1],cvScalar(thresh_saturation_min,0,0,0),cvScalar(thresh_saturation_max,0,0,0),hsv_split[1]);
 	cvInRangeS(hsv_split[2],cvScalar(thresh_value_min,0,0,0),cvScalar(thresh_value_max,0,0,0),hsv_split[2]);
-
+	cvAnd(hsv_split[0],hsv_split[1],endRecog,NULL);
+	cvAnd(endRecog,hsv_split[2],endRecog,NULL);
 
 	cvShowImage("screen",capture);
 
@@ -82,6 +87,7 @@ void process_recog()
 	cvShowImage("mat0",hsv_split[0]);
 	cvShowImage("mat1",hsv_split[1]);
 	cvShowImage("mat2",hsv_split[2]);
+	cvShowImage("end_recog",endRecog);
 #endif
 
 	cvReleaseImage(&hsv_split[0]);
