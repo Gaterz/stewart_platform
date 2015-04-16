072fa1c513a097adc5b0a79fb64030fe526021ae
gaterz <julien.maurat@free.fr>
2015-04-16 15:06:33 +0200

    + project : ajout du flag de debug de la reconnaissance d'image DEBUG_IMGRECOG % imgrecog : affichage de debug quand le flag DEBUG_IMGRECOG est defini
    Signed-off-by: julien <julien.maurat@insa-rennes.fr>

--- src/imgRecognition.c
+++ src/imgRecognition.c
@@ -20,35 +20,48 @@ void init_imgrecog(int device)
 	capture_device=cap;
 
 	cvNamedWindow("screen",CV_WINDOW_AUTOSIZE);
+	cvCreateTrackbar("thresh_hue_min", "screen", &thresh_hue_min, 255,NULL);
+	cvCreateTrackbar("thresh_hue_max", "screen", &thresh_hue_max, 255,NULL);
+
+	#ifdef DEBUG_IMGRECOG
+	cvNamedWindow("hsv",CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("mat0",CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("mat1",CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("mat2",CV_WINDOW_AUTOSIZE);
-
-	cvCreateTrackbar("thresh1", "screen", &thresh_hue_min, 255,NULL);
-	cvCreateTrackbar("thresh2", "screen", &thresh_hue_max, 255,NULL);
+	#endif
 }
 
 void exit_imgrecog()
 {
 	cvReleaseCapture(&capture_device);
 	cvDestroyWindow("screen");
+
+	#ifdef DEBUG_IMGRECOG
+	cvDestroyWindow("hsv");
+	cvDestroyWindow("mat0");
+	cvDestroyWindow("mat1");
+	cvDestroyWindow("mat2");
+	#endif
 }
 void process_recog()
 {
+	IplImage* hsv_split[4];
 	IplImage* capture=cvQueryFrame(capture_device);
-	cvCvtColor(capture,capture,CV_RGB2HSV);
-	IplImage* arr[4];
-	arr[0]=cvCreateImage(cvGetSize(capture), IPL_DEPTH_8U, 1);
-	arr[1]=cvCreateImage(cvGetSize(capture), IPL_DEPTH_8U, 1);
-	arr[2]=cvCreateImage(cvGetSize(capture), IPL_DEPTH_8U, 1);
-	cvSplit(capture,arr[0],arr[1],arr[2],0);
+	IplImage* capture_hsv=	cvCreateImage(cvGetSize(capture),capture->depth,capture->nChannels);
+	cvCvtColor(capture,capture_hsv,CV_RGB2HSV);
+	hsv_split[0]=cvCreateImage(cvGetSize(capture_hsv), IPL_DEPTH_8U, 1);
+	hsv_split[1]=cvCreateImage(cvGetSize(capture_hsv), IPL_DEPTH_8U, 1);
+	hsv_split[2]=cvCreateImage(cvGetSize(capture_hsv), IPL_DEPTH_8U, 1);
+	cvSplit(capture_hsv,hsv_split[0],hsv_split[1],hsv_split[2],0);
 
-	cvInRangeS(arr[0],cvScalar(thresh_hue_min,0,0,0),cvScalar(thresh_hue_max,0,0,0),arr[0]);
-	//cvThreshold(arr[0],arr[0],thresh_hue_min,255,CV_THRESH_BINARY);
-	//cvThreshold(arr[0],arr[0],thresh_hue_min,255,CV_THRESH_BINARY);
 
+	//cvInRangeS(hsv_split[0],cvScalar(thresh_hue_min,0,0,0),cvScalar(thresh_hue_max,0,0,0),hsv_split[0]);
 	cvShowImage("screen",capture);
-	cvShowImage("mat0",arr[0]);
-	cvShowImage("mat1",arr[1]);
-	cvShowImage("mat2",arr[2]);
+
+#ifdef DEBUG_IMGRECOG
+	cvShowImage("hsv",capture_hsv);
+	cvShowImage("mat0",hsv_split[0]);
+	cvShowImage("mat1",hsv_split[1]);
+	cvShowImage("mat2",hsv_split[2]);
+#endif
 }
